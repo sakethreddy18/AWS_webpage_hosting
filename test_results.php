708dac639cb1abc62090502cec7f7c1d3faae41f
sakethreddy18 <81903264+sakethreddy18@users.noreply.github.com>
2023-04-19 22:10:03 +0530

    Update test_results.php

--- test_results.php
+++ test_results.php
@@ -71,10 +71,10 @@
 
             </tr>
             <?php
-            $host = "localhost";
-            $dbname = "test";
-            $user = "root";
-            $pass = "";
+           $host = "cc-db-4.chr8rexir5gb.us-east-1.rds.amazonaws.com";
+           $dbname = "bloodtest";
+           $user = "admin";
+           $pass = "gsr182002";
 
 
             $mysql = mysqli_connect($host, $user, $pass, $dbname);
